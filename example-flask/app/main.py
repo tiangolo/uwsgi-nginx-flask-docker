a839a9e44f114844844a539b7f7d964917bf6a13
sebastián Ramírez <tiangolo@gmail.com>
2016-02-14 17:34:14 +0100

    Update example-flask

--- example-flask/app/main.py
+++ example-flask/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
    print "test"
-   return "Hello World"
+   return "Hello World from Flask"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True, port=80)
