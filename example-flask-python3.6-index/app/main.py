a1e786bdba7dcf688211e0039313bf63e572cfe2
Sebastián Ramírez <tiangolo@gmail.com>
2017-08-10 00:22:51 -0500

    Add static content and front end routes to SPA example

--- example-flask-python3.6-index/app/main.py
+++ example-flask-python3.6-index/app/main.py
@@ -10,5 +10,17 @@ def hello():
 def main():
     return send_file('./static/index.html')
 
+# Everything not declared before (not a Flask route / API endpoint)...
+@app.route('/<path:path>')
+def route_frontend(path):
+    # ...could be a static file needed by the front end that
+    # doesn't use the `static` path (like in `<script src="bundle.js">`)
+    file_path = './static/' + path
+    if os.path.isfile(file_path):
+        return send_file(file_path)
+    # ...or should be handled by the SPA's "router" in front end
+    else:
+        return send_file('./static/index.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
