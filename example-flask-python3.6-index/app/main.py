3d9911a6a2d24631b21850da6fe8f04787465b9e
Sebastián Ramírez <tiangolo@gmail.com>
2017-09-02 15:04:54 -0500

    Fix not imported os and format code

--- example-flask-python3.6-index/app/main.py
+++ example-flask-python3.6-index/app/main.py
@@ -1,14 +1,19 @@
+import os
+
 from flask import Flask, send_file
 app = Flask(__name__)
+
 
 @app.route("/hello")
 def hello():
     return "Hello World from Flask in a uWSGI Nginx Docker container with \
      Python 3.6 (from the example template)"
 
+
 @app.route("/")
 def main():
     return send_file('./static/index.html')
+
 
 # Everything not declared before (not a Flask route / API endpoint)...
 @app.route('/<path:path>')
@@ -21,6 +26,7 @@ def route_frontend(path):
     # ...or should be handled by the SPA's "router" in front end
     else:
         return send_file('./static/index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
