524cb008d43f9ccf2559b055649885676c1899cb
Sebastián Ramírez <tiangolo@gmail.com>
2017-09-02 15:04:38 -0500

    Format code

--- example-flask-python3.6/app/main.py
+++ example-flask-python3.6/app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-   return "Hello World from Flask in a uWSGI Nginx Docker container with \
+    return "Hello World from Flask in a uWSGI Nginx Docker container with \
      Python 3.6 (from the example template)"
 
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=80)
