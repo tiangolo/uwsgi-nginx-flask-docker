12e322d28b938d09640f29ade7ddf09bd1e0f2f5
Sebastián Ramírez <tiangolo@gmail.com>
2017-09-10 16:04:30 -0500

    Remove old dist before re-creating zip files

--- make-example-zips.sh
+++ make-example-zips.sh
@@ -4,6 +4,7 @@
 rm -rf ./**/__pycache__
 
 # Compress all the example directories in .zip files to be uploaded for a release
+rm -rf ./dist
 mkdir ./dist
 for f in example-flask* ; do
   if [[ -d $f ]] ; then
