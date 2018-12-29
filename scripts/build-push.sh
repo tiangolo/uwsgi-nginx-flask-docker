5cb33738042f7ed5d8e366bac5946c33e0da343d
Sebastián Ramírez <tiangolo@gmail.com>
2018-12-29 19:05:28 +0400

    Build and push in 2 stages, so subimages use the latest versions
    from stage 1

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,6 +4,10 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker-compose -f docker-compose.build.yml build
+docker-compose -f docker-compose.build.stage01.yml build
 
-docker-compose -f docker-compose.build.yml push
+docker-compose -f docker-compose.build.stage01.yml push
+
+docker-compose -f docker-compose.build.stage02.yml build
+
+docker-compose -f docker-compose.build.stage02.yml push
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-docker-compose -f docker-compose.build.yml build
+docker-compose -f docker-compose.build.stage01.yml build
