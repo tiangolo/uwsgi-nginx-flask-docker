a14ce2fcb0f9a97ba1b3c7b1d5d8bd0d9db9c398
Eric Boucher <eric.p.boucher@gmail.com>
2020-06-12 12:28:28 -0700

    Update Python version in example welcome message
    Update Python version to 3.8 for the welcome message in `example-flask-python3.8/app/main.py`

--- example-flask-python3.8/app/main.py
+++ example-flask-python3.8/app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.7 (from the example template)"
+     Python 3.8 (from the example template)"
 
 
 if __name__ == "__main__":
