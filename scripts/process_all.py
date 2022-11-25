49da6a31fbaf0ab475c83f90ef3ec58c28ea8dbd
Sebastián Ramírez <tiangolo@gmail.com>
2022-11-25 17:22:09 +0100

    🔥 Deprecate and remove Python 3.6 (#304)

--- scripts/process_all.py
+++ scripts/process_all.py
@@ -8,7 +8,6 @@ environments = [
     {"NAME": "python3.9", "PYTHON_VERSION": "3.9"},
     {"NAME": "python3.8", "PYTHON_VERSION": "3.8"},
     {"NAME": "python3.7", "PYTHON_VERSION": "3.7"},
-    {"NAME": "python3.6", "PYTHON_VERSION": "3.6"},
     {"NAME": "python3.8-alpine", "PYTHON_VERSION": "3.8"},
 ]
 
