b945623a336eae6c0d46bb6fcc4cc9281f6988f8
Sebastián Ramírez <tiangolo@gmail.com>
2024-03-17 07:32:37 -0500

    🔥 Remove Alpine support (#345)

--- docker-images/entrypoint.sh
+++ docker-images/entrypoint.sh
@@ -36,11 +36,4 @@ else
     printf "$content_server" > /etc/nginx/conf.d/nginx.conf
 fi
 
-# For Alpine:
-# Explicitly add installed Python packages and uWSGI Python packages to PYTHONPATH
-# Otherwise uWSGI can't import Flask
-if [ -n "$ALPINEPYTHON" ] ; then
-    export PYTHONPATH=$PYTHONPATH:/usr/local/lib/$ALPINEPYTHON/site-packages:/usr/lib/$ALPINEPYTHON/site-packages
-fi
-
 exec "$@"
